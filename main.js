c76fec399931dae118e46089d248c5cc34fc1825
“Allie <allieroll22@ufl.edu>
2019-02-17 11:13:06 -0500

    Set up placehoderText wrapper in JS

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 var button = document.getElementsByTagName("button")[0];
 
-var lunchItems = ["Mexican Food", "Italian", "Chicken", "Pizza", "Sandwiches", "Bagels", "Subs", "Wings", "Fish", "Salad", "Soup", "Flatbread"];
+var ourRandomItem = document.createElement("p");
+var lunchItemContainer = document.getElementById("generator-results");
+lunchItemContainer.appendChild(ourRandomItem);
+
+var placeholderText = document.createTextNode('');
+ourRandomItem.appendChild(placeholderText);
+
+
+var lunchItems = ["Mexican", "Italian", "Chicken", "Pizza", "Sandwiches", "Bagels", "Subs", "Wings", "Fish", "Salad", "Soup", "Flatbread"];
 
 var myFunction = function(){
   lunchItems.sort(function(a,b){return 0.5 - Math.random()});
-  console.log(lunchItems);
 };
 
 button.onclick = myFunction;
