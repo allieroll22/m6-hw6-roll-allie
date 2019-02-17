45e6fa7fcf712f5c34feaba46cffed93eb833a50
“Allie <allieroll22@ufl.edu>
2019-02-17 11:30:25 -0500

    Restyle CSS elements and add comments to JS

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 var button = document.getElementsByTagName("button")[0];
-
+//Creates Placeholder Element for Random Items
 var ourRandomItem = document.createElement("p");
+//Calls Container from HTML for items
 var lunchItemContainer = document.getElementById("generator-results");
 lunchItemContainer.appendChild(ourRandomItem);
 
-var placeholderText = document.createTextNode('');
-ourRandomItem.appendChild(placeholderText);
+//Creates Empty Placeholder Text Node for Random Item
+var placeholderNode = document.createTextNode('');
+ourRandomItem.appendChild(placeholderNode);
 
-
+//Random Items
 var lunchItems = ["Mexican", "Italian", "Chicken", "Pizza", "Sandwiches", "Bagels", "Subs", "Wings", "Fish", "Salad", "Soup", "Flatbread"];
 
+//Function
+//Returns random results into item textnode  
 var myFunction = function(){
   lunchItems.sort(function(a,b){return 0.5 - Math.random()});
   var ourRandomItemFiller = document.createTextNode(lunchItems[0]);
   ourRandomItem.appendChild(ourRandomItemFiller);
   var ourButtonSection = document.getElementById("generator-button");
   ourButtonSection.removeChild(button);
+  ourRandomItem.classList.add("button-style");
 };
 
 button.onclick = myFunction;
